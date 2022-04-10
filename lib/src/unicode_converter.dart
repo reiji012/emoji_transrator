2da8a4a7493746ccba3619f2dc35c4f088a3a8aa
reiji <reiji0120124@gmail.com>
2022-04-10 21:00:52 +0900

    create sorce file

---
+++ lib/src/unicode_converter.dart
@@ -0,0 +1,10 @@
+class UnicodeToEmoji {
+  static String toEmoji(String unicodeStr) {
+    if (unicodeStr.substring(0, 2) != 'U+') {
+      throw Exception('Unicode not detected: must start with `U+`');
+    }
+
+    final unicode = '0x${unicodeStr.substring(2, unicodeStr.length)}';
+    return String.fromCharCode(int.parse(unicode));
+  }
+}
--- lib/unicode_to_emoji.dart
+++ lib/unicode_to_emoji.dart
@@ -1,12 +1,3 @@
-library emoji_transraotr;
+library unicode_to_emoji;
 
-class UnicodeToEmoji {
-  static String toEmoji(String unicodeStr) {
-    if (unicodeStr.substring(0, 2) != 'U+') {
-      throw Exception('Unicode not detected: must start with `U+`');
-    }
-
-    final unicode = '0x${unicodeStr.substring(2, unicodeStr.length)}';
-    return String.fromCharCode(int.parse(unicode));
-  }
-}
+export 'src/unicode_converter.dart';
