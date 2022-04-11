648505a41153ea6bfb5cbe03be56ef8eb2949019
rejii <reiji0120124@gmail.com>
2022-04-11 16:23:04 +0900

    add test code

--- test/unicode_to_emoji_test.dart
+++ test/unicode_to_emoji_test.dart
@@ -5,6 +5,24 @@ import 'package:unicode_to_emoji/src/unicode_converter.dart';
 void main() {
   test('adds one to input values', () {
     expect(UnicodeToEmoji.toEmoji('U+1F600'), '😀');
-    expect(UnicodeToEmoji.toEmoji('U+1F60431'), '😀');
+
+    final unicodeList = [
+      'U+1F600',
+      'U+1F600',
+      'U+1F600',
+      'U+1F600',
+      'U+1F600',
+    ];
+
+    expect(
+      UnicodeToEmoji.toEmojiList(unicodeList),
+      [
+        '😀',
+        '😀',
+        '😀',
+        '😀',
+        '😀',
+      ],
+    );
   });
 }
